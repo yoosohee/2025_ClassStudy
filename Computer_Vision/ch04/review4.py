e15ce15d2af17b08e5c56e673b3a44a95a36d195
usag1 <144187256+yoosohee@users.noreply.github.com>
2025-09-23 11:31:44 +0900

    Update review4.py

--- Computer_Vision/ch04/review4.py
+++ Computer_Vision/ch04/review4.py
@@ -5,7 +5,7 @@ import sys
 
 img=cv.imread('cat.jpg')
 
-t, bin_img=cv.threshold(img[:,:,2], 0,255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+t, bin_img=cv.threshold(img[:,:,2], 0,255, cv.THRESH_BINARY+cv.THRESH_OTSU) # 이미지의 이진화 + 오츄 알고리즘으로 임곗값을 구해라
 print('오츄 알고리즘이 찾은 최적 임곗값= ', t)
 
 cv.imshow('R channel', img[:,:,2]) #R채널 영상
