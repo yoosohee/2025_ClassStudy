e5c4f53fc8007f551c5870d8e7bec4be8502cd02
usag1 <144187256+yoosohee@users.noreply.github.com>
2025-09-30 10:47:13 +0900

    Update review7.py

--- Computer_Vision/ch04/review7.py
+++ Computer_Vision/ch04/review7.py
@@ -8,11 +8,11 @@ img = cv.imread('cat.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap='gray'), plt.xticks([]), plt.yticks([]), plt.show()
 
-h = cv.clacHist([gray], [0], None, [256],[0,256])
+h = cv.calcHist([gray], [0], None, [256],[0,256])
 plt.plot(h,color='r', linewidth=1), plt.show()
 
 equal = cv.equalizeHist(gray)
 plt.imshow(equal, cmap='gray'),  plt.xticks([]), plt.yticks([]), plt.show()
 
-h = cv.clacHist([gray], [0], None, [256],[0,256])
+h = cv.calcHist([gray], [0], None, [256],[0,256])
 plt.plot(h,color='r', linewidth=1), plt.show()
