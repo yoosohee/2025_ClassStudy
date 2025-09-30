ec27bf9f45084b2a385ee212da5aeefbd52423ce
usag1 <144187256+yoosohee@users.noreply.github.com>
2025-09-30 10:46:30 +0900

    Update review6.py

--- Computer_Vision/ch04/review6.py
+++ Computer_Vision/ch04/review6.py
@@ -8,7 +8,7 @@ img  = cv.resize(img, dsize = (0,0), fx=0.25, fy=0.25)
 
 def gamma(f, gamma=1.0):
     f1 = f/255.0
-    return np.unit8(255*(f1**gamma))
+    return np.uint8(255*(f1**gamma))
 
 gc = np.hstack((gamma(img, 0.5), gamma(img, 0.75), gamma(img, 1.0), gamma(img, 2.0), gamma(img, 3.0)))
 cv.imshow('gamma', gc)
